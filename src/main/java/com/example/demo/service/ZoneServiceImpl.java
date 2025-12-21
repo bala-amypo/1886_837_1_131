5c4ab8b288d7a180789715dc818be51f2f928ffa
bala-amypo <bala@amypo.in>
2025-12-21 09:04:01 +0000

    User commit

--- src/main/java/com/example/demo/service/ZoneServiceImpl.java
+++ src/main/java/com/example/demo/service/ZoneServiceImpl.java
@@ -1,10 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Zone;
-import com.example.demo.exception.BadRequestException;
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.ZoneRepository;
-
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,26 +15,11 @@ public class ZoneServiceImpl {
         this.repo = repo;
     }
 
-    public Zone createZone(Zone z) {
-        if (repo.findByZoneName(z.getZoneName()).isPresent()) {
-            throw new BadRequestException("Zone name must be unique");
-        }
+    public Zone create(Zone z) {
         return repo.save(z);
-    }
-
-    public Zone get(Long id) {
-        return repo.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Zone not found"));
     }
 
     public List<Zone> getAll() {
         return repo.findAll();
-    }
-
-    public Zone deactivate(Long id) {
-        Zone z = get(id);
-        z.setActive(false);
-        return repo.save(z);
     }
 }
