c1cfb3523ed695206283829470839b6990fa1914
bala-amypo <bala@amypo.in>
2025-12-24 07:44:47 +0000

    User commit

--- src/main/java/com/example/demo/service/DemandReadingServiceImpl.java
+++ src/main/java/com/example/demo/service/DemandReadingServiceImpl.java
@@ -1,10 +1,3 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.DemandReading;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
 @Service
 public class DemandReadingServiceImpl {
 
