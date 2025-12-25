24c506d324b86d50e22a9bc16aa6786fa6d195ea
bala-amypo <bala@amypo.in>
2025-12-25 17:08:13 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,10 +1,11 @@
+
+
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -12,9 +13,8 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                // You need to change the port as per your server
-                .servers(List.of(
-                        new Server().url("https://9213.408procr.amypo.ai/")
-                ));
-        }
+                .info(new Info()
+                        .title("Smart Grid Load Shedding API")
+                        .version("1.0"));
+    }
 }
