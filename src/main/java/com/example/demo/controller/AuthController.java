364eff57d275f8f01ee00594468c0d3ac94ee69b
bala-amypo <bala@amypo.in>
2025-12-26 16:39:55 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.*;
+import com.example.demo.entity.AppUser;
+import com.example.demo.exception.BadRequestException;
+import com.example.demo.repository.AppUserRepository;
+import com.example.demo.security.JwtTokenProvider;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AppUserRepository repo;
+    private final PasswordEncoder encoder;
+    private final JwtTokenProvider provider;
+
+    public AuthController(
+            AppUserRepository repo,
+            PasswordEncoder encoder,
+            JwtTokenProvider provider) {
+        this.repo = repo;
+        this.encoder = encoder;
+        this.provider = provider;
+    }
+
+    @PostMapping("/register")
+    public AppUser register(@RequestBody AuthRequest req) {
+
+        if (repo.findByEmail(req.getEmail()).isPresent())
+            throw new BadRequestException("unique");
+
+        AppUser user = AppUser.builder()
+                .email(req.getEmail())
+                .password(encoder.encode(req.getPassword()))
+                .role("ROLE_USER")
+                .active(true)
+                .build();
+
+        return repo.save(user);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody AuthRequest req) {
+
+        AppUser user = repo.findByEmail(req.getEmail())
+                .orElseThrow(() -> new BadRequestException("Invalid credentials"));
+
+        if (!encoder.matches(req.getPassword(), user.getPassword()))
+            throw new BadRequestException("Invalid credentials");
+
+        String token = provider.createToken(user);
+
+        return new AuthResponse(
+                token,
+                user.getId(),
+                user.getEmail(),
+                user.getRole()
+        );
+    }
+}
